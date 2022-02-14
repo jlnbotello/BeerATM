70f0dc8896a1b7f9012740f5efd42980bf3d5125
jlnbo <jlnbotello@gmail.com>
2022-02-14 10:40:09 -0300

    Change dispenser interface

--- include/dispenser/dispenser.hh
+++ include/dispenser/dispenser.hh
@@ -2,18 +2,31 @@
 #define DISPENSER_CLASS_H
 
 #include "valve.hh"
+#include "tank.hh"
+#include "flowmeter.hh"
 
 namespace beer_atm
 {
+    struct Price{
+        float value;
+        std::string unit;
+    };
+    struct Drink{
+        std::string name;
+        std::string desc;
+        Price price;
+    };
+    
     class Dispenser
     {
     public:
-        Dispenser(Valve & v);
-        void EnableOutput();
-        void DisableOutput();    
-        void ResetAccumulator();
-        void AddToAccumulator(int mililitres);
-        void GetAccumulator();
+        Dispenser(int id, Drink d, Valve & v, Tank & t, Flowmeter & f);
+        void Enable();
+        void Disable();
+        Drink GetDrink();
+        void OnDeliveryStart(/* callback */);
+        void OnDeliveryStop(/* callback */);
+        void OnDeliveryUpdate(/* callback */ int threshold);
     };
 } // namespace beer_atm
 
